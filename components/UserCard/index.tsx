8710bcd502c5bc4808ba8325461d3b2de57240f1
gusdecante <gustavo.antunes@fatecitapetininga.edu.br>
2021-05-17 08:26:42 -0300

    Third attempt for redeploy

--- components/UserCard/index.tsx
+++ components/UserCard/index.tsx
@@ -31,7 +31,7 @@ const UserCard = () => {
     callComments,
     deletePost,
   } = useContext(UserFileContext)
-  const files = []
+  let files: [{ source: string; id: number }] | undefined
 
   const handleViewer = (selected: any) =>
     setViewer({ selected, visible: !viewer.visible })
